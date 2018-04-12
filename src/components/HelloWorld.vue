62e2d6c6ed5cb1a7b8abcad4e0bac84d867dc2c3
hhwcode <1021457010@qq.com>
2018-04-12 22:42:58 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,56 @@
 <template>
-    <div>
-      <el-row>hello vue</el-row>
-      <el-row>
-        <el-col :span="8">
-            <el-input v-model="input" placeholder="请输入内容"></el-input>
-            <div>{{input}}</div>
-        </el-col>
-        <el-col><el-input v-model="input" placeholder="请输入内容"></el-input></el-col>
-      </el-row>
-    </div>
+  <div>
+    <el-row>hello vue</el-row>
+    <el-row>
+      <el-col :span="8">
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      input: ''
+  export default {
+    name: "HelloWorld",
+    data() {
+      return {
+        input: ""
+      };
     }
-  }
-}
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1,h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
 </style>
