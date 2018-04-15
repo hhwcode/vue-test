cc1482ba9b3da2724d943650379415d15a59e89b
hhwcode <1021457010@qq.com>
2018-04-15 21:52:14 +0800

    修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,10 @@
   <div>
     <el-row>hello vue</el-row>
     <el-row>
-      <el-col :span="8">
+      <el-col>
+        <el-input v-model="input" placeholder="请输入内容"></el-input>
+      </el-col>
+      <el-col>
         <el-input v-model="input" placeholder="请输入内容"></el-input>
       </el-col>
       <el-col>
